5699ea9ea2569aca048c17ad56b0db4b595c8d0a
zBl4ck <zBl4ckDev@protonmail.com>
2023-07-23 23:11:42 -0300

    Create fernet key if it not exist

--- src/pwd_treat.py
+++ src/pwd_treat.py
@@ -1,5 +1,17 @@
 from cryptography.fernet import Fernet
 
+def new_key():
+    key = Fernet.generate_key()
+    file = open("key.txt", "wb")
+    file.write(key)
+    file.close()
+    return key
+
+def check_if_key_exist():
+    import os.path
+    if not os.path.isfile("key.txt"):
+        new_key()
+
 def get_key():
     file = open("key.txt", "r") 
     key = file.readline()
@@ -8,12 +20,14 @@ def get_key():
 
 def crypt_password(password):
     password = password.encode()
+    check_if_key_exist()
     key = bytes(get_key().encode("utf-8"))
     cipher_suite = Fernet(key)
     ciphered_text = cipher_suite.encrypt(password)
     return ciphered_text
     
 def decrypt_password(encrypted_pwd):
+    check_if_key_exist()
     key = bytes(get_key().encode("utf-8"))
     cipher_suite = Fernet(key)
     pwd_bytes = cipher_suite.decrypt(encrypted_pwd)
